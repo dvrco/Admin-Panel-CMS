48d0889e50b65d787a555d6237938e464656ba6e
Józef Dabiri <yusdabiri@gmail.com>
2017-10-30 23:50:32 +0100

    Add project files.

---
+++ ShopingCardCMS/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,31 @@
+﻿using ShopingCardCMS.Models.Data;
+using ShopingCardCMS.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopingCardCMS.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            // Declare list of PageVM
+            List<PageVM> pagesList;
+            
+
+            using(Db db = new Db())
+            {
+                //Init the list
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+
+            }
+
+            // Return view with list
+            return View(pagesList);
+        }
+    }
+}
